edc5126e1755ecaf9aeabdae0c98ba7d30318a24
Deniz Toprak <deniztoprak@users.noreply.github.com>
2022-10-12 20:48:00 +0200

    Add stake function

--- contracts/Pool.sol
+++ contracts/Pool.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
-// import "hardhat/console.sol";
+import "hardhat/console.sol";
 
 /**
  * @title Pool contract
@@ -15,8 +15,27 @@ contract Pool {
     // Use SafeERC20 to deal with non-reverting / non-standard ERC20 tokens
     using SafeERC20 for IERC20;
 
+    // Events
+    event Staked(address indexed account, uint256 amount);
+
+    // State variables
+
     IERC20 public rewardToken;
     uint8 public apr;
+
+    struct Stake {
+        uint256 balance;
+        uint256 lastStakeTime;
+        uint256 dueReward;
+    }
+
+    uint256 private _totalSupply;
+    mapping(address => Stake) private _stakes;
+
+    /**
+     * @dev Reward token address is injected in constuctor to prevent tight coupling with reward implementation
+     * (provides modularity and saves gas)
+     */
 
     constructor(address _rewardToken, uint8 _apr) {
         require(_rewardToken != address(0), "Reward token address can not be zero");
@@ -25,5 +44,66 @@ contract Pool {
         apr = _apr;
     }
 
-    function createPool() public {}
+    // View functions
+
+    /**
+     * @notice Returns total liquidity supply of the contract
+     */
+    function totalSupply() external view returns (uint256) {
+        return _totalSupply;
+    }
+
+    /**
+     * @notice Returns the balance of an account
+     */
+    function balanceOf(address account) external view returns (uint256) {
+        return _stakes[account].balance;
+    }
+
+    /**
+     * @notice Returns the reward of an account
+     * @dev View functions don't cost gas when called externally
+     */
+    function rewardOf(address account) external view returns (uint256) {
+        return _stakes[account].dueReward + calculateReward(account);
+    }
+
+    /**
+     * @notice Returns the due reward
+     */
+    function calculateReward(address account) internal view returns (uint256) {
+        uint256 yearlyInterest = (_stakes[account].balance * apr) / 100;
+        uint256 stakedDay = (block.timestamp - _stakes[account].lastStakeTime) / 60 / 60 / 24;
+        // console.log("Balance", _stakes[account].balance);
+        // console.log("Yearly interest", yearlyInterest);
+        // console.log("Last stake time", _stakes[account].lastStakeTime);
+        // console.log("Block time", block.timestamp);
+        // console.log("Time diff", block.timestamp - _stakes[account].lastStakeTime);
+        // console.log("Staked Day", stakedDay);
+        // console.log("Reward", (yearlyInterest / 360) * stakedDay);
+
+        // Assume the APR is for 360 days instead of 365
+        // 360 is a highly composite number which leads to more precise results
+        // https://en.wikipedia.org/wiki/Highly_composite_number
+        return (yearlyInterest / 360) * stakedDay;
+    }
+
+    // Mutative functions
+
+    /**
+     * @notice Stake given amount of liquidity
+     */
+    function stake() external payable {
+        require(msg.value >= 5 ether, "Staking amount should be minimum 5 ETH");
+        if (_stakes[msg.sender].balance != 0) {
+            // If user has already staked: calculate the reward, store it in dueReward and restart stake time
+            // This prevents false reward calculation when user adds liquidity incrementally
+            _stakes[msg.sender].dueReward = calculateReward(msg.sender);
+        }
+
+        _stakes[msg.sender].balance += msg.value;
+        _stakes[msg.sender].lastStakeTime = block.timestamp;
+        _totalSupply += msg.value;
+        emit Staked(msg.sender, msg.value);
+    }
 }
--- test/Pool.ts
+++ test/Pool.ts
@@ -1,6 +1,11 @@
 import { loadFixture } from '@nomicfoundation/hardhat-network-helpers';
 import { expect } from 'chai';
 import { ethers } from 'hardhat';
+import { time } from '@nomicfoundation/hardhat-network-helpers';
+
+const ONE_DAY_IN_SECONDS = 60 * 60 * 24;
+// Assume the APR is for 360 days instead of 365
+const ONE_YEAR_IN_SECONDS = ONE_DAY_IN_SECONDS * 360;
 
 describe('Pool', function () {
   // Define a fixture to reuse the same setup
@@ -23,11 +28,12 @@ describe('Pool', function () {
   describe('Deployment', function () {
     it('sets the reward token address', async function () {
       const { PoolInstance, USDCTokenInstance } = await loadFixture(deployContracts);
+      const rewardToken = await PoolInstance.rewardToken();
 
-      expect(await PoolInstance.rewardToken()).to.equal(USDCTokenInstance.address);
+      expect(rewardToken).to.equal(USDCTokenInstance.address);
     });
 
-    it('throws when the reward token address is not provided', async function () {
+    it('reverts when the reward token address is not provided', async function () {
       const Pool = await ethers.getContractFactory('Pool');
       await expect(Pool.deploy(ethers.constants.AddressZero, '123')).to.be.revertedWith(
         'Reward token address can not be zero'
@@ -36,22 +42,111 @@ describe('Pool', function () {
 
     it('sets the annual percentage rate', async function () {
       const { PoolInstance, PoolAPR } = await loadFixture(deployContracts);
+      const apr = await PoolInstance.apr();
 
-      expect(await PoolInstance.apr()).to.equal(PoolAPR);
+      expect(apr).to.equal(PoolAPR);
     });
 
-    it('throws when the annual percentage rate is below 1', async function () {
+    it('reverts when the annual percentage rate is below 1', async function () {
       const Pool = await ethers.getContractFactory('Pool');
       const USDCTokenAddress = '0x1111111111111111111111111111111111111111';
       const PoolAPR = ethers.BigNumber.from('0');
       await expect(Pool.deploy(USDCTokenAddress, PoolAPR)).to.be.revertedWith('APR must be between 1 and 100');
     });
 
-    it('throws when the annual percentage rate is above 100', async function () {
+    it('reverts when the annual percentage rate is above 100', async function () {
       const Pool = await ethers.getContractFactory('Pool');
       const USDCTokenAddress = '0x1111111111111111111111111111111111111111';
       const PoolAPR = ethers.BigNumber.from('101');
       await expect(Pool.deploy(USDCTokenAddress, PoolAPR)).to.be.revertedWith('APR must be between 1 and 100');
+    });
+  });
+
+  describe('Staking', function () {
+    it("updates users' balance when they stake ether", async function () {
+      const [deployer, user1, user2] = await ethers.getSigners();
+      const user1StakedValue = ethers.utils.parseEther('5');
+      const user2StakedValue = ethers.utils.parseEther('10');
+      const { PoolInstance } = await loadFixture(deployContracts);
+      await PoolInstance.connect(user1).stake({ value: user1StakedValue });
+      await PoolInstance.connect(user2).stake({ value: user2StakedValue });
+      const user1Balance = await PoolInstance.balanceOf(user1.address);
+      const user2Balance = await PoolInstance.balanceOf(user2.address);
+
+      expect(user1Balance).to.equal(user1StakedValue);
+      expect(user2Balance).to.equal(user2StakedValue);
+    });
+
+    it('updates total supply when users stake ether', async function () {
+      const [deployer, user1, user2] = await ethers.getSigners();
+      const user1StakedValue = ethers.utils.parseEther('5');
+      const user2StakedValue = ethers.utils.parseEther('10');
+      const { PoolInstance } = await loadFixture(deployContracts);
+      await PoolInstance.connect(user1).stake({ value: user1StakedValue });
+      // await time.increaseTo(1696979854);
+      // await PoolInstance.connect(user1).stake({ value: user1StakedValue });
+      await PoolInstance.connect(user2).stake({ value: user2StakedValue });
+      const totalSupply = await PoolInstance.totalSupply();
+
+      expect(totalSupply).to.equal(user1StakedValue.add(user2StakedValue));
+    });
+
+    it('updates reward when users stake', async function () {
+      const [deployer, user1, user2] = await ethers.getSigners();
+      const user1StakedValue = ethers.utils.parseEther('36');
+      const user2StakedValue = ethers.utils.parseEther('72');
+      const { PoolInstance, PoolAPR } = await loadFixture(deployContracts);
+      // Initial stake
+      await PoolInstance.connect(user1).stake({ value: user1StakedValue });
+      await PoolInstance.connect(user2).stake({ value: user2StakedValue });
+      // Fast forward time
+      await time.increase(ONE_YEAR_IN_SECONDS);
+
+      const user1Reward = await PoolInstance.rewardOf(user1.address);
+      const user2Reward = await PoolInstance.rewardOf(user2.address);
+      const user1ExpectedReward = user1StakedValue.mul(PoolAPR).div(100);
+      const user2ExpectedReward = user2StakedValue.mul(PoolAPR).div(100);
+
+      expect(user1Reward).to.equal(user1ExpectedReward);
+      expect(user2Reward).to.equal(user2ExpectedReward);
+    });
+
+    it('updates reward when users increase their stake', async function () {
+      const [deployer, user1] = await ethers.getSigners();
+      const user1StakedValue = ethers.utils.parseEther('36');
+      const { PoolInstance, PoolAPR } = await loadFixture(deployContracts);
+      // Initial stake
+      await PoolInstance.connect(user1).stake({ value: user1StakedValue });
+      // Fast forward time
+      await time.increase(ONE_YEAR_IN_SECONDS);
+      // Additional stake
+      await PoolInstance.connect(user1).stake({ value: user1StakedValue });
+      await time.increase(ONE_YEAR_IN_SECONDS / 2);
+
+      const user1Reward = await PoolInstance.rewardOf(user1.address);
+      const expectedReward = user1StakedValue.mul(2).mul(PoolAPR).div(100);
+
+      expect(user1Reward).to.equal(expectedReward);
+    });
+
+    it('emits "staked" event when users stake', async function () {
+      const [deployer, user1] = await ethers.getSigners();
+      const user1StakedValue = ethers.utils.parseEther('36');
+      const { PoolInstance } = await loadFixture(deployContracts);
+
+      await expect(PoolInstance.connect(user1).stake({ value: user1StakedValue }))
+        .to.emit(PoolInstance, 'Staked')
+        .withArgs(user1.address, user1StakedValue);
+    });
+
+    it('reverts when user staked less than 5 ETH', async function () {
+      const [deployer, user1] = await ethers.getSigners();
+      const user1StakedValue = ethers.utils.parseEther('4.9');
+      const { PoolInstance } = await loadFixture(deployContracts);
+
+      await expect(PoolInstance.connect(user1).stake({ value: user1StakedValue })).to.be.revertedWith(
+        'Staking amount should be minimum 5 ETH'
+      );
     });
   });
 });
